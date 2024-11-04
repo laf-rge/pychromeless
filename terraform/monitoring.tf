63a3502a773014b4601df298d97dff151cf90d9b
William N. Green <william.green@gmail.com>
2024-11-04 06:31:27 -0800

    monitoring for failures

---
+++ terraform/monitoring.tf
@@ -0,0 +1,61 @@
+locals {
+  monitored_functions = toset([
+    "daily-sales",
+    "email-tips",
+    "third-party-deposit",
+    "invoice-sync",
+    "daily_journal",
+    "transform_tips",
+    "get_mpvs"
+  ])
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  for_each = local.monitored_functions
+
+  alarm_name          = "${each.key}-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "0"
+  alarm_description   = "This metric monitors ${each.key} lambda function errors"
+
+  dimensions = {
+    FunctionName = each.key
+  }
+
+  alarm_actions = [aws_sns_topic.lambda_alerts.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_duration" {
+  for_each = local.monitored_functions
+
+  alarm_name          = "${each.key}-duration"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = "300"
+  statistic           = "Maximum"
+  threshold           = "270000" # 270 seconds (90% of 5 minute timeout)
+  alarm_description   = "Alert when ${each.key} lambda function is approaching timeout"
+
+  dimensions = {
+    FunctionName = each.key
+  }
+
+  alarm_actions = [aws_sns_topic.lambda_alerts.arn]
+}
+
+resource "aws_sns_topic" "lambda_alerts" {
+  name = "lambda-monitoring-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.lambda_alerts.arn
+  protocol  = "email"
+  endpoint  = "william@wagonermanagement.com"
+}
