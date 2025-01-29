5a065b0c8272517e0c31bc30fea013520f86f0f5
William N. Green <william.green@gmail.com>
2025-01-28 18:10:01 -0800

    remove D&D vendor

--- src/qb.py
+++ src/qb.py
@@ -609,7 +609,6 @@ def vendor_lookup(gl_vendor_name):
         refresh_session()
         vendor = {
             "WNEPLS": Vendor.where("DisplayName like 'The Paper%'", qb=CLIENT)[0],
-            "PR-D&D": Vendor.where("DisplayName like 'D&D%'", qb=CLIENT)[0],
             "PEPSI": Vendor.where("DisplayName like 'Pepsi%'", qb=CLIENT)[0],
             "GenPro": Vendor.where("DisplayName like 'General Produce'", qb=CLIENT)[0],
             "SYSFRA": Vendor.where("DisplayName like 'Sysco San%'", qb=CLIENT)[0],
