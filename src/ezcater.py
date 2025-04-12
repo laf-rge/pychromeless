bacedb4c3ce59bf6b8829b3a51bcbcf58174ff25
William N. Green <william.green@gmail.com>
2025-04-12 10:29:24 -0700

    graphql on ezcater

--- src/ezcater.py
+++ src/ezcater.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import json
 from time import sleep
 from typing import cast
 
@@ -21,7 +22,7 @@ class EZCater:
     def _login(self):
         self._driver = initialise_driver()
         driver = self._driver
-        driver.implicitly_wait(25)
+        driver.implicitly_wait(5)
         driver.set_page_load_timeout(45)
 
         # logout
@@ -44,28 +45,134 @@ class EZCater:
         results = []
         driver = self._driver
         sleep(5)
+
+        # Navigate to payments page to ensure we're in the right context
         driver.get("https://ezmanage.ezcater.com/payments")
         WebDriverWait(driver, 45)
-        apollo = driver.execute_script("return window.__APOLLO_STATE__")
-        payment_ids = []
-        for key in apollo.keys():
-            if key.startswith("CatererVendorPayment"):
-                payment_ids.append(key.split(":")[1])
 
-        for pid in payment_ids[::-1]:
-            driver.get(f"https://ezmanage.ezcater.com/payments/{pid}")
-            WebDriverWait(driver, 45)
-            apollo = driver.execute_script("return window.__APOLLO_STATE__")
-            data = apollo[f"CatererVendorPayment:{pid}"]
-            result = self.extract_deposit(data)
-            if (
-                result[4] in stores
-                and result[1] >= start_date
-                and result[1] <= end_date
-            ):
+        # GraphQL query for payments list
+        list_query = {
+            "operationName": "PaymentsPaymentsTablePaymentsQuery",
+            "variables": {
+                "direction": "DESC",  # Get newest first
+                "field": "SENT_ON",
+                "limit": 100,  # Increased limit to get more results at once
+                "offset": 0,
+            },
+            "query": """
+                query PaymentsPaymentsTablePaymentsQuery($offset: Int, $limit: Int, $field: VendorPaymentSortField!, $direction: SortDirection!) {
+                    me {
+                        catererAccount {
+                            billing {
+                                vendorPayments(
+                                    offset: $offset
+                                    limit: $limit
+                                    orderBy: {field: $field, direction: $direction}
+                                ) {
+                                    totalCount
+                                    edges {
+                                        node {
+                                            id
+                                            sentOn
+                                            name
+                                            __typename
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            """,
+        }
+
+        # Execute GraphQL query for payment list
+        response = driver.execute_script(
+            """
+            return fetch('https://ezmanage-api.ezcater.com/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*',
+                },
+                credentials: 'include',
+                body: arguments[0]
+            }).then(response => response.json());
+        """,
+            json.dumps([list_query]),
+        )
+
+        # Extract payment IDs and process them
+        payments_data = response[0]["data"]["me"]["catererAccount"]["billing"][
+            "vendorPayments"
+        ]["edges"]
+
+        for payment in payments_data:
+            payment_node = payment["node"]
+            payment_id = payment_node["id"]
+            payment_date = datetime.datetime.strptime(
+                payment_node["sentOn"], "%Y-%m-%d"
+            ).date()
+            store_id = payment_node["name"].split("#")[1][:5]
+
+            if store_id in stores and start_date <= payment_date <= end_date:
+                # GraphQL query for detailed payment info
+                detail_query = {
+                    "operationName": "PaymentDetailsPaymentsQuery",
+                    "variables": {"paymentId": payment_id},
+                    "query": """
+                        query PaymentDetailsPaymentsQuery($paymentId: ID!) {
+                            me {
+                                catererAccount {
+                                    billing {
+                                        vendorPayment(id: $paymentId) {
+                                            id
+                                            sentOn
+                                            name
+                                            accountingTotals {
+                                                food
+                                                salesTax
+                                                deliveryFees
+                                                marketplaceCommission
+                                                tips
+                                                creditCardFees
+                                                miscFees
+                                                __typename
+                                            }
+                                            __typename
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    """,
+                }
+
+                # Execute GraphQL query for payment details
+                detail_response = driver.execute_script(
+                    """
+                    return fetch('https://ezmanage-api.ezcater.com/graphql', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': '*/*',
+                        },
+                        credentials: 'include',
+                        body: arguments[0]
+                    }).then(response => response.json());
+                """,
+                    json.dumps([detail_query]),
+                )
+
+                payment_data = detail_response[0]["data"]["me"]["catererAccount"][
+                    "billing"
+                ]["vendorPayment"]
+                result = self.extract_deposit(payment_data)
                 results.extend([result])
             else:
-                logger.warning(f"skipping {result[4]} {result[1]}")
+                # logger.warning(f"skipping {store_id} {payment_date}")
+                continue
+
         return results
 
     def extract_deposit(self, data):
